10e3985666edf47215575852170d5112874770b3
TsuyoshiNumano <numano.tsuyoshi@lancers.co.jp>
2017-06-15 10:07:38 +0900

    chore: add settings

---
+++ webpack.config.base.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const webpack = require('webpack');
+const FlowStatusWebpackPlugin = require('flow-status-webpack-plugin');
+
+require('dotenv').config();
+
+const config = {
+    entry: ['./src/index.js'],
+    output: {
+        path: path.resolve('www/js/'),
+        filename: 'bundle.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                enforce: 'pre',
+                use: [{
+                    loader: 'eslint-loader',
+                    options: {
+                        configFile: './.eslintrc.yaml',
+                        fix: true,
+                    },
+                }],
+            },
+            {
+                test: /\.js$/,
+                use: 'babel-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    plugins: [
+        new webpack.NoEmitOnErrorsPlugin(),
+        new FlowStatusWebpackPlugin({
+            binaryPath: './node_modules/.bin/flow',
+            failOnError: true,
+        }),
+        new webpack.DefinePlugin({
+            'process.env': {
+                'NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+            },
+        }),
+    ],
+};
+
+module.exports = config;
---
+++ webpack.config.development.js
@@ -0,0 +1,36 @@
+const webpack = require('webpack');
+const merge = require('webpack-merge');
+const path = require('path');
+const WriteFilePlugin = require('write-file-webpack-plugin');
+const baseConfig = require('./webpack.config.base.js');
+
+const config = merge.smart(baseConfig, {
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: path.join(__dirname, './www'),
+        port: 8080,
+        host: '0.0.0.0',
+        inline: true,
+        hot: true,
+        stats: {
+            assets: true,
+            children: false,
+            chunks: false,
+            hash: false,
+            modules: false,
+            publicPath: false,
+            timings: true,
+            version: false,
+            warnings: true,
+            colors: {
+                green: '\u001b[32m',
+            },
+        },
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new WriteFilePlugin(),
+    ],
+});
+
+module.exports = config;
